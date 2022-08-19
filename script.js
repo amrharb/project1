6dc74b511cbb2475443ad9cbce4aea4e78304572
Amr Harb <amrharb001@gmail.com>
2022-08-19 23:06:55 +0200

    final version of project1 of Udemy Home Page

--- script.js
+++ script.js
@@ -1,5 +1,50 @@
 const courses="http://localhost:3000/";
 let group="python";
+let details={
+    "aws":{
+        "name": "AWS",
+        "title": "Top courses in AWS Certification",
+        "header": "Become an expert in cloud computing with AWS Certification",
+        "description": "Prep for your AWS certification with an AWS course on Udemy. Learn the fundamentals of AWS such as working with a serverless platform, the various frameworks, security and more. With these courses, you’ll build the valuable skills you need to implement cloud initiatives — and open up new career opportunities. If you want to become an AWS developer, we’ve got the course for you.",
+    },
+    "data":{
+        "name":"Data Science",
+        "title": "Top courses in Data Science",
+        "header": "Lead data-driven decisions with Data Science",
+        "description": "Data science application is an in-demand skill in many industries worldwide — including finance, transportation, education, manufacturing, human resources, and banking. Explore data science courses with Python, statistics, machine learning, and more to grow your knowledge. Get data science training if you’re into research, statistics, and analytics.",    
+    },
+    "draw":{
+        "name": "Drawing",
+        "title": "Top courses in Drawing",
+        "header": "Expand your creative skillset with Drawing",
+        "description": "Want to start drawing for fun or take your craft to the next level? Explore our online drawing classes and learn pencil drawing, figure drawing, cartoon drawing, character drawing for cartoons and anime, illustration, sketching, shading and more. Take an overview course on the fundamentals of drawing or zero in on an area you’d like to improve with a specialized course. We’ve got tons of options to get — and keep — you going.",    
+    },
+    "js":{
+        "name": "JavaScript",
+        "title": "Top courses in JavaScript",
+        "header": "Grow your software development skills with JavaScript", 
+        "description": "JavaScript is a text-based computer programming language used to make dynamic web pages. A must-learn for aspiring web developers or programmers, JavaScript can be used for features like image carousels, displaying countdowns and timers, and playing media on a webpage. With JavaScript online classes, you can learn to build",    
+    },
+    "python":{
+        "name": "Python",
+        "title": "Top courses in Python",
+        "header":"Expand your career opportunities with Python",
+        "description":"Take one of Udemy’s range of Python courses and learn how to code using this incredibly useful language. Its simple syntax and readability makes Python perfect for Flask, Django, data science, and machine learning. You’ll learn how to build everything from games to sites to apps. Choose from a range of courses that will appeal to both beginners and advanced developers alike.",    
+    },
+    "web":{
+        "name": "Web Development",
+        "title": "Top courses in Web Development",
+        "header": "Build websites and applications with Web Development",
+        "description": "The world of web development is as wide as the internet itself. Much of our social and vocational lives play out on the internet, which prompts new industries aimed at creating, managing, and debugging the websites and applications that we increasingly rely on.",    
+    },
+    "excel":{
+        "name": "Excel",
+        "title": "Top courses in Excel",
+        "header": "Analyze and visualize data with Excel",
+        "description":"Take a Microsoft Excel course from Udemy, and learn how to use this industry-standard software. Real-world experts will show you the basics like how to organize data into sheets, rows and columns, and advanced techniques like creating complex dynamic formulas. Both small businesses and large companies use Excel to turn their raw data into actionable insights.",    
+    }
+}
+
 function drawCourse(course){
     let rate="";
     for(let i=1;i<=5;i++){
@@ -21,9 +66,9 @@ function drawCourse(course){
         index++;
     }
     return `
-    <div class="col-3">
-        <div class="card">
-            <img class="d-block w-100 card-img-top" src="${course.image}" alt="python course">
+    <div class="col-xl-3 col-lg-4 col-md-6 col-sm-10">
+        <div class="card h-100">
+            <img class="d-block w-100 h-100 card-img-top" src="${course.image}" alt="python course">
             <div class="card-body">
                <h4 class="card-title">${course.title}</h4>
                <p class="card-text">${instructor}</p>
@@ -42,8 +87,16 @@ async function getCourses(searchKey) {
         let search=`${courses}${group}`;
         let obj = await fetch(search);
         let json = await obj.json();
+        let cur=details[group];
         let content=
         `
+       <div class="desktop">
+            <h3>${cur.header}</h3>
+            <p>${cur.description}</p>
+            <div class="my-4">
+                <button><b>Explore ${cur.name}</b></button>
+            </div>
+        </div>
         <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
         <div class="carousel-item active">
@@ -69,11 +122,11 @@ async function getCourses(searchKey) {
             </div>
         </div>
         <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="carousel-control-prev-icon slider bg-dark" aria-hidden="true"></span>
             <span class="sr-only">Previous</span>
         </a>
         <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="carousel-control-next-icon slider bg-dark" aria-hidden="true"></span>
             <span class="sr-only">Next</span>
         </a>
     </div>
